9ca360dc7fb283f95db5ec4b3839ee420e19876d
gera <gera.weiss@gmail.com>
2025-10-11 12:36:55 +0300

    Feat: add keyboard shortcuts for navigation using arrow keys

---
+++ setup/shortcuts.ts
@@ -0,0 +1,18 @@
+import type { NavOperations, ShortcutOptions } from '@slidev/types'
+import { defineShortcutsSetup } from '@slidev/types'
+
+export default defineShortcutsSetup((nav: NavOperations, base: ShortcutOptions[]) => {
+  return [
+    ...base, // keep the existing shortcuts
+    {
+      key: 'arrowleft',
+      fn: () => nav.next(),
+      autoRepeat: true,
+    },
+    {
+      key: 'arrowright',
+      fn: () => nav.prev(),
+      autoRepeat: true,
+    },
+  ]
+})
